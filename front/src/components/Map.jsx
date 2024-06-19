f5a4b1a19a046af16b75c76f3075137353f9f4e1
kurukuruzz <112538549+kurukuruzz@users.noreply.github.com>
2024-06-19 21:22:35 +0900

    style: 지역별 키워드 수 추가, 마우스 이벤트 수정

--- front/src/components/Map.jsx
+++ front/src/components/Map.jsx
@@ -1,15 +1,16 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
+import Papa from "papaparse";
 
 const Map = () => {
   const [rowLevel, setRowLevel] = useState(13);
   const [level, setLevel] = useState(13);
-  const [mapCenter, setMapCenter] = useState({
+  let [mapCenter, setMapCenter] = useState({
     lat: 36.03257609653945,
     lng: 127.95172999438276,
   });
   const [mapInstance, setMapInstance] = useState(null);
-  const [address, setAddress] = useState('');
+  const [hotTopics, setHotTopics] = useState([]);
 
   useEffect(() => {
     const script = document.createElement("script");
@@ -21,7 +22,9 @@ const Map = () => {
 
     script.onload = () => {
       console.log("Kakao Map script loaded");
-      window.kakao.maps.load(initMap);
+      if (hotTopics.length > 0) {
+        window.kakao.maps.load(initMap);
+      }
     };
     script.onerror = () => {
       console.error("Failed to load the Kakao Map script");
@@ -30,51 +33,100 @@ const Map = () => {
     return () => {
       document.head.removeChild(script);
     };
+  }, [hotTopics]);
+
+  useEffect(() => {
+    fetchCSVData();
   }, []);
+
 
   useEffect(() => {
     setRowLevel(level);
-
+    console.log(level);
     if ((level <= 10 && rowLevel >= 11) || (level >= 11 && rowLevel <= 10)) {
       window.kakao.maps.load(initMap);
     }
-
-    if (mapInstance) {
-      if (level >= 13) {
-        mapInstance.setDraggable(false);
-      } else {
-        mapInstance.setDraggable(true);
-        mapInstance.setZoomable(true);
-      }
-    }
+    
   }, [level]);
 
-  useEffect(() => {
-    if (mapInstance) {
-      window.kakao.maps.event.addListener(mapInstance, "click", async (e) => {
-        const { latLng } = e;
-        handleAreaClick(latLng);
-        const result = await fetch(
-          `https://dapi.kakao.com/v2/local/geo/coord2address.json?x=${latLng.La}&y=${latLng.Ma}`,
-          {
-            method: "get",
-            headers: {
-              Authorization: `KakaoAK b7cf9eae2097956579182491f68a9d5e`,
-              KA: "sdk/1.0.0 os/javascript lang/en-KR origin/http://localhost:3000",
-            },
-          }
-        );
+  const handleMapClick = (e) => {
+    const clickedLat = e.latLng.getLat();
+    const clickedLng = e.latLng.getLng();
+    updateMapCenter(clickedLat, clickedLng);
+    console.log(11111111)
+  };
+  const updateMapCenter = (lat, lng) => {
+    setMapCenter({ lat, lng });
+  };
 
-        const data = await result.json();
-        if (data.documents.length > 0) {
-          const addressInfo = data.documents[0].address.address_name.split(" ");
-          setAddress(`${addressInfo[0]} ${addressInfo[1]}`);
-        } else {
-          console.error("주소 정보를 찾을 수 없습니다.");
+  useEffect(() => {
+  if (mapInstance) {
+    const clickListener = window.kakao.maps.event.addListener(mapInstance, "click", async (e) => {
+      const { latLng } = e;
+      // handleAreaClick(latLng);
+      const result = await fetch(
+        `https://dapi.kakao.com/v2/local/geo/coord2address.json?x=${latLng.La}&y=${latLng.Ma}`,
+        {
+          method: "get",
+          headers: {
+            Authorization: `KakaoAK b7cf9eae2097956579182491f68a9d5e`,
+            KA: "sdk/1.0.0 os/javascript lang/en-KR origin/http://localhost:3000",
+          },
         }
+      );
+      const data = await result.json();
+      console.log(data);
+    });
+
+    const dblClickListener = window.kakao.maps.event.addListener(mapInstance, "dblclick", (e) => {
+      handleMapClick(e);
+      const { latLng } = e;
+      if (mapInstance.getLevel() === 10) {
+        mapInstance.setLevel(13);
+      } else {
+        mapInstance.setLevel(10);
+        mapInstance.setCenter(new window.kakao.maps.LatLng(latLng.getLat(), latLng.getLng()));
+      }
+    });
+
+    // Cleanup event listeners on component unmount
+    return () => {
+      window.kakao.maps.event.removeListener(mapInstance, "click", clickListener);
+      window.kakao.maps.event.removeListener(mapInstance, "dblclick", dblClickListener);
+    };
+  }
+}, [mapInstance]);
+
+  
+
+  const fetchCSVData = async () => {
+    try {
+      const response = await fetch('city_keyword.csv'); // CSV 파일 경로
+      const csvData = await response.text();
+      const parsedData = Papa.parse(csvData, { header: true }).data;
+
+      const cityMap = {};
+      parsedData.forEach(row => {
+        const { id, keyword, city } = row;
+        if (!cityMap[city]) {
+          cityMap[city] = [];
+        }
+        cityMap[city].push({ id, keyword });
       });
+
+      const hotTopicsList = Object.keys(cityMap).map(city => ({
+        city,
+        hotTopics: cityMap[city].slice(0, 3)
+      }));
+
+      setHotTopics(hotTopicsList);
+      if (hotTopicsList.length > 0) {
+        window.kakao.maps.load(initMap);
+      }
+    } catch (error) {
+      console.error("Failed to fetch CSV data", error);
     }
-  }, [mapInstance]);
+  };
 
   const initMap = () => {
     if (window.kakao && window.kakao.maps) {
@@ -92,6 +144,11 @@ const Map = () => {
         const map = new window.kakao.maps.Map(container, options);
 
         setMapInstance(map);
+        // 맵의 확대/축소 이벤트 막기
+        map.setZoomable(false);
+        
+        // 맵의 드래그 이동 막기
+        // map.setDraggable(false);
 
         window.kakao.maps.event.addListener(map, "zoom_changed", function () {
           const currentLevel = map.getLevel();
@@ -102,8 +159,8 @@ const Map = () => {
           const latlng = map.getCenter();
           setMapCenter({ lat: latlng.getLat(), lng: latlng.getLng() });
         });
+        { !!hotTopics && loadGeoJson(map, level); }
 
-        loadGeoJson(map, level);
       } else {
         console.error("Map container not found");
       }
@@ -143,7 +200,14 @@ const Map = () => {
             polygon.setOptions({ fillColor: "#fff" })
           );
 
-          const regionName = properties.CTP_KOR_NM || properties.SIG_KOR_NM;
+          let regionName = properties.CTP_KOR_NM || properties.SIG_KOR_NM;
+
+          // hotTopics 리스트에서 city에 해당하는 regionName과 매칭되는 항목 찾기
+          const hotTopicItem = hotTopics.find(item => item.city === regionName);
+          if (hotTopicItem) {
+            const hotTopicsCount = hotTopicItem.hotTopics.length;
+            regionName = `${regionName}: ${hotTopicsCount}개의 키워드`;
+          }
 
           // 중앙 좌표 계산 (모든 좌표의 평균)
           let latSum = 0;
@@ -163,7 +227,7 @@ const Map = () => {
 
           const customOverlay = new window.kakao.maps.CustomOverlay({
             position: center,
-            content: `<div style="padding:5px;background:white;border:1px solid black;">${regionName}</div>`,
+            content: `<div style="padding:5px;background:white;border:1px solid black;font-size:10px;">${regionName}</div>`,
           });
 
           customOverlay.setMap(map);
@@ -174,27 +238,9 @@ const Map = () => {
       );
   };
 
-  const handleAreaClick = (latLng) => {
-    const latitude = latLng.getLat();
-    const longitude = latLng.getLng();
-
-    if (mapInstance) {
-      if (!isNaN(latitude) && !isNaN(longitude)) {
-        const targetCoords = new window.kakao.maps.LatLng(latitude, longitude);
-        mapInstance.setCenter(targetCoords);
-        mapInstance.setLevel(9); 
-      } else {
-        console.error("유효하지 않은 좌표입니다:", latitude, longitude);
-      }
-    } else {
-      console.error("mapInstance가 유효하지 않습니다.");
-    }
-  };
-
   return (
     <Container>
       <div id="map" className="map"></div>
-      <div>{address}</div> {/* 선택된 주소 표시 */}
     </Container>
   );
 };
